d8d66646ae483b5a596e5ee8364899e872b8c932
unknown <eddiesef@hotmail.com>
2015-09-04 11:34:03 -0700

    changed the program

--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -1,16 +1,32 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Lightning extends PApplet {
+
 int startX=150;
 int startY=90;
 int endX=150;
 int endY=90;
 PImage photo;
-void setup()
+public void setup()
 {
 	size(500,500);
 	strokeWeight(1);
 	background(0,0,0);
 	photo=loadImage("volibear.png");
 }
-void draw()
+public void draw()
 {
 	fill(0,0,0,20);
 	rect(0,0,500,500);
@@ -50,3 +66,12 @@ startX=(int)(Math.random()*500);
 
 	
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Lightning" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
